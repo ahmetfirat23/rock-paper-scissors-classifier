73dedeea1271f3399ba97fcdd336e73ddd416506
Ahmet Firat Gamsiz <ahmetfiratgamsiz287@gmail.com>
2023-01-31 22:37:53 +0300

    Model selection added

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def load_model():
 
     x = layers.Flatten()(last_output)
     x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dense(512, activation='relu')(x)
     x = layers.Dense(3, activation='softmax')(x)
 
     model = Model(pre_trained_model.input, x)
@@ -46,12 +47,54 @@ def load_model():
     model.compile()
     return model
 
+@st.cache(allow_output_mutation=True)
+def load_model_2():
+    img_input = layers.Input(shape=(150, 150, 3))
+
+    x = tf.image.rgb_to_grayscale(img_input)
+
+    x = layers.Conv2D(32, 3, activation="relu", kernel_regularizer="l2")(x)
+    x = layers.MaxPooling2D(2)(x)
+    x = layers.LeakyReLU(0.01)(x)
+
+    x = layers.Conv2D(64, 3, activation="relu", kernel_regularizer="l2")(x)
+    x = layers.MaxPooling2D(2)(x)
+    x = layers.LeakyReLU(0.01)(x)
+
+    x = layers.Conv2D(128, 3, activation="relu", kernel_regularizer="l2")(x)
+    x = layers.MaxPooling2D(2)(x)
+    x = layers.LeakyReLU(0.01)(x)
+
+    x = layers.Flatten()(x)
+    x = layers.Dense(1024, activation="relu", activity_regularizer="l2")(x)
+    x = layers.Dropout(0.2)(x)
+
+    x = layers.Dense(512, activation="relu", activity_regularizer="l2")(x)
+    x = layers.Dropout(0.2)(x)
+
+    output = layers.Dense(3, activation="softmax")(x)
+
+    model = Model(img_input, output)
+    model.load_weights("my_model_2.hdf5")
+    model.compile()
+    return model
 
 images = load_images()
 my_model = load_model()
+my_model_2 = load_model_2()
+current_model = my_model
+
 st.title("Rock Paper Scissors")
 st.header("Play rock paper scissors using your camera")
-col1, col2 = st.columns(2)
+
+radio = st.radio("Current model", ["Inception_v3","CNN"], horizontal=True)
+if radio == "Inception_v3":
+    current_model = my_model
+elif radio == "CNN":
+    current_model = my_model_2
+
+col1, col2 = st.columns([1.3,1], gap="large")
+
 with col1:
     picture = st.camera_input("Take a picture, keep your hand close to the camera")
 if picture is not None:
@@ -62,7 +105,7 @@ if picture is not None:
 
     data = tf.reshape(resizing_layer, shape=(1, 150, 150, 3))
 
-    prediction = my_model.predict(data)
+    prediction = current_model.predict(data)
     with col2:
         pred = np.argmax(prediction)
         move = (pred + 1) % 3
